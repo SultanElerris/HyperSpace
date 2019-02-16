bc5008bb31b01c86964c49d3c7db0c4e655fd0a5
sultan elerris <s.elerris@me.com>
2019-02-16 15:14:51 -0500

    Adding files back

---
+++ HyperSpace/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HyperSpace
+//
+//  Created by Sultan Sultan on 2/16/19.
+//  Copyright © 2019 Sultan Sultan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,8 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '10.0'
+use_frameworks!
+
+target 'HyperSpace' do
+    pod 'Alamofire', '~> 5.0.0-beta.2'
+    pod 'GoogleAnalytics'
+    end
